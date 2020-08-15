44a03f78f0c864bc9e46e40e0960b17af854a9ac
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-08-16 04:06:34 +0530

    Try To resolve stdlib.h error mac runners and renamed Runner Path

---
+++ configure/build-arrow-mac.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export CPATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include
+mkdir arrow/cpp/build
+cmake -S arrow/cpp/  -B arrow/cpp/build \
+                -DARROW_BUILD_SHARED=OFF \
+                -DARROW_BUILD_STATIC=ON \
+                -DARROW_COMPUTE=ON \
+                -DARROW_CSV=ON \
+                -DARROW_DATASET=ON \
+                -DARROW_DEPENDENCY_SOURCE=BUNDLED \
+                -DARROW_DEPENDENCY_USE_SHARED=OFF \
+                -DARROW_FILESYSTEM=ON \
+                -DARROW_HDFS=ON \
+                -DARROW_JEMALLOC=ON \
+                -DARROW_JSON=ON \
+                -DARROW_ORC=ON \
+                -DARROW_PARQUET=ON \
+                -DARROW_PLASMA=ON \
+                -DARROW_WITH_BROTLI=ON \
+                -DARROW_WITH_BZ2=ON \
+                -DARROW_WITH_LZ4=ON \
+                -DARROW_WITH_SNAPPY=ON \
+                -DARROW_WITH_ZLIB=ON \
+                -DARROW_WITH_ZSTD=ON \
+                -DORC_SOURCE=BUNDLED \
+                -DCMAKE_C_COMPILER=gcc-9.3 \
+                -DCMAKE_CXX_COMPILER=g++-9.3
+
+make  arrow/cpp/build/Makefile
+
+make install -C arrow/cpp/build/
+
--- configure/macos-env.sh
+++ configure/macos-env.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 brew install boost
-export CPATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include

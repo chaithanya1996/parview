0ad729e7fd48847ceafe8e37b08d4ca71e68804d
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-08-15 19:36:55 +0530

    Fixed Builds and trying build process

--- configure/ubuntu-env.sh
+++ configure/ubuntu-env.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install -y -V ca-certificates lsb-release wget
-if [ $(lsb_release --codename --short) = "stretch" ]; then
-   tee /etc/apt/sources.list.d/backports.list <<APT_LINE
-deb http://deb.debian.org/debian $(lsb_release --codename --short)-backports main
-APT_LINE
-fi
-wget https://apache.bintray.com/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb
-apt install -y -V ./apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb
-apt update
-
-apt install -y -V libarrow-dev # For C++
-apt install -y -V libarrow-dataset-dev # For Arrow Dataset C++
-apt install -y -V libarrow-flight-dev # For Flight C++
-apt install -y -V libplasma-dev # For Plasma C++
-apt install -y -V libgandiva-dev # For Gandiva C++
-apt install -y -V libparquet-dev # For Apache Parquet C++
-apt install -y -V libboost-all-dev
-apt install -y -V cmake
+apt-get update -y -q && \
+    apt-get install -y -q --no-install-recommends \
+      build-essential \
+      cmake && \
+    apt-get clean && rm -rf /var/lib/apt/lists*

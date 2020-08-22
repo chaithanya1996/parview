2d270b468750f5bf42e9c9c247f23ee4fb575bea
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-08-23 01:52:05 +0530

    Mac Build Change compiler

--- configure/build-arrow-mac.sh
+++ configure/build-arrow-mac.sh
@@ -10,20 +10,9 @@ cmake -S arrow/cpp/  -B arrow/cpp/build \
                 -DARROW_DATASET=ON \
                 -DARROW_DEPENDENCY_SOURCE=BUNDLED \
                 -DARROW_DEPENDENCY_USE_SHARED=OFF \
-                -DARROW_FILESYSTEM=ON \
-                -DARROW_HDFS=ON \
-                -DARROW_JEMALLOC=ON \
-                -DARROW_JSON=ON \
-                -DARROW_ORC=ON \
                 -DARROW_PARQUET=ON \
-                -DARROW_PLASMA=ON \
-                -DARROW_WITH_BROTLI=ON \
-                -DARROW_WITH_BZ2=ON \
-                -DARROW_WITH_LZ4=ON \
-                -DARROW_WITH_SNAPPY=ON \
-                -DARROW_WITH_ZLIB=ON \
-                -DARROW_WITH_ZSTD=ON \
-                -DORC_SOURCE=BUNDLED
+                -DCMAKE_C_COMPILER=/usr/local/bin/gcc-9.3.0 \
+                -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-9.3.0
 
 make  arrow/cpp/build/Makefile
 

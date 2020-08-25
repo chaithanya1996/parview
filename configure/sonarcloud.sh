855561e2d8be9c11bd4073c1b6740cc66f035936
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-08-25 10:57:34 +0530

    sonarcloud integration

---
+++ configure/sonarcloud.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export SONAR_SCANNER_VERSION=4.4.0.2170
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+curl --create-dirs -sSLo $HOME/.sonar/build-wrapper-linux-x86.zip https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -o $HOME/.sonar/build-wrapper-linux-x86.zip -d $HOME/.sonar/
+export PATH=$HOME/.sonar/build-wrapper-linux-x86:$PATH
+
+build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory cmake --build parbuild/ --config Release
+
+sonar-scanner \
+  -Dsonar.organization=chaithanya1996 \
+  -Dsonar.projectKey=chaithanya1996_parview \
+  -Dsonar.sources=. \
+  -Dsonar.cfamily.build-wrapper-output=bw-output \
+  -Dsonar.host.url=https://sonarcloud.io

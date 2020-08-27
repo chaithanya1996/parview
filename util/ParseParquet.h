6b856d92b6183730876f69e49e165834d23430c8
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-08-27 05:59:55 +0530

    Updated Definitions
    Signed-off-by: A.V.S.Chaitanya <chaithanya1996@outlook.com>

--- util/ParseParquet.h
+++ util/ParseParquet.h
@@ -4,11 +4,11 @@
 
 #ifndef PARVIEW_PARSEPARQUET_H
 #define PARVIEW_PARSEPARQUET_H
+
 #include <arrow/api.h>
-#include <arrow/io/api.h>
-#include <parquet/arrow/reader.h>
 #include "string"
 
+using arrow::Table;
 using std::string;
 
 class ParseParquet {
@@ -17,8 +17,8 @@ public:
     static void getHead(const string& path, int counH = 50);
     static void getTail(const string& path, int coun = 50);
     static std::vector<int> getTableFormatters();
-    static void writeHeader(arrow::Table table);
-    static void writeTable(arrow::Table table);
+    static void writeHeader(std::shared_ptr<Table> table);
+    static void writeTable(std::shared_ptr<Table> table);
 };
 
 

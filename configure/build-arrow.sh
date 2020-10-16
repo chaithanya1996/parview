43b64643815de13317b0cc842dd6dd1e06efdd26
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2020-10-17 05:02:38 +0530

    Added Compression Libraries to build

--- configure/build-arrow.sh
+++ configure/build-arrow.sh
@@ -8,10 +8,14 @@ cmake -S arrow/cpp/  -B build/arrow \
                 -DARROW_CSV=ON \
                 -DARROW_DEPENDENCY_SOURCE=BUNDLED \
                 -DARROW_DEPENDENCY_USE_SHARED=OFF \
-                -DARROW_PARQUET=ON
-
+                -DARROW_PARQUET=ON \
+                -DARROW_WITH_BROTLI=ON \
+                -DARROW_WITH_BZ2=ON \
+                -DARROW_WITH_LZ4=ON \
+                -DARROW_WITH_SNAPPY=ON \
+                -DARROW_WITH_ZLIB=ON \
+                -DARROW_WITH_ZSTD=ON
 
 make -j2 -C build/arrow
-
 make install -C build/arrow
 

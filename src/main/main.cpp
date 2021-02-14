f111754d85045d63eec5528c591c95d8f80e287e
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2021-02-14 22:10:30 +0530

    Moving away from Program options

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,8 +4,9 @@
 #include "util/ParseParquet.h"
 #include <fstream>
 #include <filesystem>
+#include <cxxopts.hpp>
 
-namespace po = boost::program_options;
+namespace po = cxxopts;
 namespace fs = std::filesystem;
 
 using std::cout;
@@ -16,57 +17,49 @@ int main(int argc, char *argv[] ) {
     // Declare the supported options.
 
     // Declare the Class
-    po::options_description manOptions("Manual for parview utility");
+    cxxopts::Options manOptions("Manual for parview utility");
 
-    int headC=50;
-    int tailC=50;
     // Add Supported options
     manOptions.add_options()
             ("help", "See available Commands and the usage")
-            ("head,h", po::value<int>(&headC)->implicit_value(50), "No of lines to be displayed from the start of the parquet file \n Default Value is 50")
-            ("tail,t", po::value<int>(&tailC)->implicit_value(50), "No of lines to be displayed till the bottom of the parquet file \n Default Value is 50")
-            ("csv,c",po::value<bool>(),"Convert the parquet file to csv file with delimiter ,")
-            ("filename,f",po::value<string>(),"specify the parquet file name")
-            ("output,o",po::value<string>(),"specify the output file name");
+            ("head,h", "No of lines to be displayed from the start of the parquet file \n Default Value is 50", po::value<int>()->default_value("50"))
+            ("tail,t",  "No of lines to be displayed till the bottom of the parquet file \n Default Value is 50",po::value<int>()->default_value("50"))
+            ("csv,c","Convert the parquet file to csv file with delimiter ,",po::value<bool>()->default_value("false"))
+            ("filename,f","specify the parquet file name",po::value<string>())
+            ("output,o","specify the output file name",po::value<string>());
 
 
-    po::positional_options_description positionalGuide;
-    positionalGuide.add("filename", -1);
+    manOptions.parse_positional("filename");
 
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(manOptions).positional(positionalGuide).run(), vm);
+    po::ParseResult parsedManOptions = manOptions.parse(argc, argv);
 
-
-    po::notify(vm);
-
-    if (vm.count("help") || vm.count("filename") == 0) {
-        cout << manOptions << "\n";
+    if (parsedManOptions.count("help") || parsedManOptions.count("filename") == 0) {
+        cout << manOptions.help() << "\n";
         return 1;
     }
 
-    if (vm.count("head")){
-        ParseParquet::getHead(vm.at("filename").as<string>(),headC);
+    if (parsedManOptions.count("head")){
+        ParseParquet::getHead(parsedManOptions["filename"].as<string>(),parsedManOptions["head"].as<int>());
         return 0;
     }
 
-    if (vm.count("tail")){
-        ParseParquet::getTail(vm.at("filename").as<string>(),tailC);
+    if (parsedManOptions.count("tail")){
+        ParseParquet::getTail(parsedManOptions["filename"].as<string>(),parsedManOptions["tail"].as<int>());
         return 0;
     }
 
     // Convert to String in CSV
-    auto filePath =  vm.at("filename");
-    string outPutString =  ParseParquet::getCsv(filePath.as<string>()) ;
+    string outPutString =  ParseParquet::getCsv(parsedManOptions["filename"].as<string>()) ;
 
     // write to output
     std::ofstream outputCSV;
     string oFileName;
 
     try{
-       string oFileName = vm.at("output").as<string>();
+       string oFileName = parsedManOptions["output"].as<string>();
     }
     catch(const std::exception& e){
-        oFileName = fs::path(filePath.as<string>()).stem();
+        oFileName = fs::path(parsedManOptions["output"].as<string>()).stem();
     }
     
     
